1e1877d5b0dd69f13d855b0629ce06915a147399
jdhofmann <justindhofmann@gmail.com>
2021-03-26 10:37:44 -0400

    lowest price

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,5 +1,9 @@
 
 export default function ProductCard({product}){
+
+    const findLowestPrice = () => {
+        return product.skus.reduce((min, p) => p.price < min ? p.price : min, product.skus[0].price);
+    }
     return (
         <div 
             key={product.id}
@@ -7,6 +11,7 @@ export default function ProductCard({product}){
         >
             <h3><span>{product.brand} - </span>{product.name}</h3>
             <img style={{ width: "100px"}} src={product.frontimg}></img>
+            <p>from {findLowestPrice()}$</p>
         </div>
     )
 }
