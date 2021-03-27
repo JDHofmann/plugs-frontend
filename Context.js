c4359fa0609cb5d547996122521c3b3679122119
jdhofmann <justindhofmann@gmail.com>
2021-03-27 16:56:39 -0400

    product page has context

--- Context.js
+++ Context.js
@@ -3,4 +3,5 @@ import React from 'react';
 
 const Context = React.createContext({});
 
+
 export default Context;
--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,12 +1,26 @@
 import withContext from "../../withContext";
 import { useRouter } from 'next/router'
 
-function Product(){
+function Product({ context }){
     const router = useRouter()
     // use the same naming convention as inside []'s for file
     const { productId } = router.query;
+
+    const findProduct = () => {
+      let product = context.products.filter(p => p.id === parseInt(productId))
+      console.log(product[0])
+      return product[0]
+    }
+    
     return (
-        <div>{productId}</div>
+        <>
+        { context.products.length > 0 ? 
+        <>
+        <h1>{findProduct().id}</h1>
+        <p>{findProduct().additional_specs}</p>
+        </>
+        : null }
+        </>
     )
 }
 
