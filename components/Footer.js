ddfcfb5afcdd7d3ead66a1bcecadae51b391e537
jdhofmann <justindhofmann@gmail.com>
2021-03-31 14:02:55 -0400

    footer tweak

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer(){
         <footer>
             <div>
                 <h4>C&G</h4>
-                <ul>
-                    <li>623 New York Ave</li>
-                    <li>New York, NY 10000</li>
-                    <li>(555) 555-555</li>
+                <ul className="address">
+                    <li><p>623 New York Ave</p></li>
+                    <li><p>New York, NY 10000</p></li>
+                    <li><p>(555) 555-555</p></li>
                 </ul>
                 <span>made by JDBH</span>
             </div>
