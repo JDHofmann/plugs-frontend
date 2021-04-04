ce333fe4d18d7738dce51bd7199d40f7a4078b26
jdhofmann <justindhofmann@gmail.com>
2021-04-04 16:54:20 -0400

    scrolling links actually work

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,52 @@
-import CategoryContainer from "../components/CategoryContainer"
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Layout from "../components/Layout";
 import withContext from "../withContext";
 
+
 function Home({context}) {
 
-  const scrollLinks = ( i=0 ) => {
-    let links = document.querySelectorAll(".link")
-    links.forEach( lx => lx.classList.remove("in-view"))
-    if(i === links.length ) i = 0;
-    links[i].classList.add("in-view")
-    i+=1;
-    setTimeout(() => scrollLinks(i), 7000)
-}
+  const [current, setCurrent] = useState(0);
+
+  const links = [
+    { name: "laptops",
+      url: "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"
+    },
+    { name: "phones",
+      url: "https://brain-images-ssl.cdn.dixons.com/3/8/10215583/u_10215583.jpg"
+    },
+    { name: "tablets",
+      url: "https://economictimes.indiatimes.com/thumb/msid-76216026,width-1200,height-900,resizemode-4,imgsize-235016/surface-pro-7-does-everything-that-a-tablet-should-do-and-doubles-up-as-a-really-efficient-laptop-.jpg?from=mdr"
+    }
+  ]
+
+  const renderLinks = () => {
+    return links.map( l => (
+      <div className={ current === links.indexOf(l) ? "link in-view": "link"}>
+        <img
+          alt=""
+          className="link-img"
+          src={l.url}
+        />
+        <Link href={"/category/" + l.name} >
+          <a className="category-link" >Shop {l.name}</a>
+        </Link>
+      </div>
+    ))
+  }
+
+  useEffect( () => {
+    let next;
+    current === links.length - 1 ? next = 0 : next = (current + 1);
+    const id = setTimeout(() => setCurrent(next), 2000);
+    return () => clearTimeout(id);
+  }, [current]);
+
   return (
     <Layout>
       <div>
         <div className="scrolling-links-container">
-              <div className="link in-view">
-                  <div id="a" className="link-background"></div>
-                  <p>Shop Laptops</p>
-              </div>
-              <div className="link">
-                  <div id="b" className="link-background"></div>
-                  <p>Shop Smartphones</p>
-              </div>
-              <div className="link">
-                  <div id="c" className="link-background"></div>
-                  <p>Shop Tablets</p>
-              </div>
+          {renderLinks()}
         </div>
         <div className="mission">
             <p>Find the electronics you deserve</p>
