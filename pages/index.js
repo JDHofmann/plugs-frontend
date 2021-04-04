177b5b098f57603a54c3c365edd632fec69838f4
jdhofmann <justindhofmann@gmail.com>
2021-04-04 13:20:08 -0400

    slider added to homepage, not rotating pics yet

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,36 @@ import withContext from "../withContext";
 
 function Home({context}) {
 
-  const renderProductByCategory = (category) => {
-    return context.products.filter( p => p.category === category)
-  }
+  const scrollLinks = ( i=0 ) => {
+    let links = document.querySelectorAll(".link")
+    links.forEach( lx => lx.classList.remove("in-view"))
+    if(i === links.length ) i = 0;
+    links[i].classList.add("in-view")
+    i+=1;
+    setTimeout(() => scrollLinks(i), 7000)
+}
   return (
     <Layout>
       <div>
-        <h1>Home</h1>
-        <CategoryContainer id="phones" title="Phones" products={renderProductByCategory("phone")}/>
-        <CategoryContainer id="tablets" title="Tablets" products={renderProductByCategory("tablet")}/>
-        <CategoryContainer id="laptops" title="Laptops" products={renderProductByCategory("laptop")}/>
-        <CategoryContainer id="smart-wathces" title="Smart Watches" products={renderProductByCategory("smart watch")}/>
-        <CategoryContainer id="tvs" title="TV's" products={renderProductByCategory("tv")}/>
-        <CategoryContainer id="accessories" title="Accessories" products={renderProductByCategory("accessory")}/>
+        <div className="scrolling-links-container">
+              <div className="link in-view">
+                  <div id="a" className="link-background"></div>
+                  <p>Shop Laptops</p>
+              </div>
+              <div className="link">
+                  <div id="b" className="link-background"></div>
+                  <p>Shop Smartphones</p>
+              </div>
+              <div className="link">
+                  <div id="c" className="link-background"></div>
+                  <p>Shop Tablets</p>
+              </div>
+        </div>
+        <div className="mission">
+            <p>Find the electronics you deserve</p>
+        </div>
       </div>
-      </Layout>
+    </Layout>
   )
 }
 
