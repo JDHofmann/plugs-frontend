d725b6870c86786b84845cc191cb2e1e34946461
jdhofmann <justindhofmann@gmail.com>
2021-04-05 10:08:25 -0400

    confirmation modal styled

--- components/ConfirmationModal.js
+++ components/ConfirmationModal.js
@@ -12,12 +12,19 @@ export default function ConfirmationModal({reciept, setConfirmationModal}){
         <>
             <div className="modal-mask"></div>
             <div className="confirmation-modal">
+                { reciept ? 
+                <>
                 <h1>Order Confirmation</h1>
-                <p>Order Number: {reciept.order.id}</p>
+                <p>Thank you for your order!</p>
+                <p>Your Order Number is: {reciept.order.id}</p>
+                <p>You will recieve a confirmation email shortly</p>
                 <button
                     onClick={handleContinueToShop}
                     className="continue-to-shop"
                 >Continue to Shop</button>
+                </>
+                : null
+                }
             </div>
         </>
     )
